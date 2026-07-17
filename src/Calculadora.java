c45c279677cda49f5cc8787d865ebc24b93aff31
Maurilio <maurilio_ramos@estudante.sesisenai.org.br>
2026-07-16 22:08:55 -0300

    Calculadora total

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Calculadora {
-    public static void main(String[] args){
-        System.out.println("Esta e o meu segundo projeto");
-    }
+
+    static void soma(int n1, int n2) {
+        System.out.println("O soma é: " + (n1 + n2));
+        }
+    static void subtracao(int n1,int n2){
+        System.out.println("A divisão é: " + (n1 + n2));
+        }
+    static void multiplicao(int n1,int n2){
+        System.out.println("A multiplicção é: " + (n1 + n2));
+        }
+    static void divisao(int n1,int n2){
+            if(n2==0){
+                System.out.println("Não é possível por zero");
+            }else{
+                System.out.println("A divisão é " + (n1/n2));
+            }
+         }
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int n1 = leia.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int n2 = leia.nextInt();
+
+        soma(n1,n2);
+        divisao(n1,n2);
+        multiplicao(n1,n2);
+        subtracao(n1,n2);
+
+        }
+
 }
